a4c85c0afea5de011fa71206ecd906e749eb1e49
sebhz <seb@zyglute.fr>
2010-01-26 21:27:09 +0100

    Added basic command line parsing

--- src/yambe.c
+++ src/yambe.c
@@ -1,19 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <getopt.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include "SDL/SDL_gfxPrimitives.h"
+
+#define VERSION_STRING "1.0"
 
 typedef struct {
 	double x;
 	double y;
 } point_t;
 
-static int nx = 640, ny = 400;
-static int nmax = 100;
+typedef struct {
+	int nx; /* X resolution of window */
+	int ny; /* Y resolution of window */
+	int nmax; /* Number of iterations to perform before auuming divergence */
+} settings_t;
+
+static settings_t settings;
+
 const char *WINDOW_TITLE = "Mandelbrot";
 static int *colormap;
+
+void usage(char *prog_name, FILE *stream) {
+	fprintf(stream, "%s (version %s):\n", prog_name, VERSION_STRING);
+	fprintf(stream, "\t--version      | -v: show program version\n");
+	fprintf(stream, "\t--help         | -h: show this help\n");
+	fprintf(stream, "\t--n_iterations | -n: number of iterations to perform before assuming divergence\n");
+	fprintf(stream, "\t--geometry=<geo>  | -g: sets the window geometry.\n\n");
+}
+
+void default_settings(void) 
+{
+	settings.nmax = 1024;
+	settings.nx = 640; 
+	settings.ny = 480; 
+}
+
+int set_geometry(char *s) {
+	int l, i, spos = -1;
+
+	l = strlen(s);
+	if (l == 0) {
+		fprintf(stderr, "Missing geometry definition\n");
+		return -1;
+	}
+
+	for (i=0; i < l; i++) {
+		if ( ((s[i] < '0') || (s[i] > '9')) && (s[i] != 'x') ) {
+			fprintf(stderr, "Wrong character found in geometry definition at position %d: %c\n", i, s[i]);
+			return -1;
+		}
+		if (s[i] == 'x') {
+			if (spos != -1) {
+				fprintf(stderr, "Bad geometry definition string (contains at least two 'x')\n");
+				return -1;
+			}
+			if (i == 0) {
+				fprintf(stderr, "Bad geometry definition string (missing x dimension)\n");
+				return -1;
+			}
+			s[i] = '\0';
+			spos = i;
+		}
+	}
+	if (spos == -1) {
+		fprintf(stderr, "Bad geometry definition\n");
+		return -1;
+	}
+
+	settings.nx = atoi(s);
+	settings.ny = atoi(s+spos+1);
+	return 0;
+
+}
+
+void parse_options (int argc, char **argv)
+{
+  int c;
+
+  while (1)
+    {
+      static struct option long_options[] =
+        {
+          /* These options set a flag. */
+          {"version",      no_argument, 0, 'v'},
+          {"help",         no_argument, 0, 'h'},
+          {"n_iterations", required_argument, 0, 'n'},
+          {"geometry",     required_argument, 0, 'g'},
+          {0, 0, 0, 0}
+        };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
+
+      c = getopt_long (argc, argv, "vhn:g:",
+                       long_options, &option_index);
+
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 'h':
+			usage(argv[0], stdout);
+			exit(EXIT_SUCCESS);
+			break;
+
+        case 'v':
+			fprintf(stdout, "%s\n", VERSION_STRING);
+			exit(EXIT_SUCCESS);
+			break;
+
+        case 'n':
+			settings.nmax = atoi(optarg);
+			break;
+
+		case 'g':
+			if (set_geometry(optarg)) exit(EXIT_FAILURE);
+			break;
+
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+       while (optind < argc)
+			fprintf(stderr, "%s is not recognized as a valid option or argument\n", argv[optind++]);
+		usage(argv[0], stderr);
+	}
+}
 
 void
 apply_surface (int x, int y, SDL_Surface * source, SDL_Surface * destination)
@@ -31,12 +156,12 @@ void mandelbrot(point_t *center, double width, int *res)
 	int i, j, n;
 	
 	xmin = center->x-width/2;
-	ymax = center->y+width/2*ny/nx;
-	step = width/nx;
+	ymax = center->y+width/2*settings.ny/settings.nx;
+	step = width/settings.nx;
 	
-	for (j=0; j<ny; j++) {
+	for (j=0; j<settings.ny; j++) {
 		b = ymax-j*step;
-		for (i=0; i < nx; i++) {
+		for (i=0; i < settings.nx; i++) {
 			a = i*step+xmin;
 			x=0; y=0; n=0;
 			do {
@@ -44,8 +169,8 @@ void mandelbrot(point_t *center, double width, int *res)
 				y  = 2*x*y+b;
 				x  = x1;
 				n++;
-			} while (((x*x+y*y) < 4) && (n <= nmax));
-			res[j*nx + i] = n;
+			} while (((x*x+y*y) < 4) && (n <= settings.nmax));
+			res[j*settings.nx + i] = n;
 		}
 	}
 }
@@ -55,7 +180,7 @@ SDL_Surface *init_SDL(void)
 	SDL_Surface *s;
 
 	SDL_Init(SDL_INIT_VIDEO);
-    s = SDL_SetVideoMode (nx, ny, 0,
+    s = SDL_SetVideoMode (settings.nx, settings.ny, 0,
                           SDL_HWSURFACE | SDL_DOUBLEBUF |
                           SDL_RESIZABLE);
     SDL_WM_SetCaption (WINDOW_TITLE, 0);
@@ -112,9 +237,9 @@ void display_screen(SDL_Surface *s, int *res)
 {
 	int x, y, n;
 
-	for (x=0; x< nx; x++)
-		for (y=0; y<ny; y++) {
-			n = res[y*nx+x];
+	for (x=0; x< settings.nx; x++)
+		for (y=0; y<settings.ny; y++) {
+			n = res[y*settings.nx+x];
 			DrawPixel(s, x, y, colormap[n] >> 16, (colormap[n] >> 8)& 0x0000FF, (colormap[n] >> 16)& 0x0000FF);
 	}
 	SDL_Flip(s);
@@ -128,19 +253,22 @@ int main(int argc, char **argv)
 	SDL_Surface *screen;
 	
     srand (time (NULL));
-	if ((res = (int *)malloc(nx*ny*sizeof(int))) == NULL ) {
+	default_settings();
+	parse_options(argc, argv);
+
+	if ((res = (int *)malloc(settings.nx*settings.ny*sizeof(int))) == NULL ) {
 		fprintf(stderr, "Unable to allocate memory for screen buffer\n");
 		exit(EXIT_FAILURE);
 	}
 	
-	if ((colormap = (int *)malloc((nmax+1)*sizeof(int))) == NULL ) {
+	if ((colormap = (int *)malloc((settings.nmax+1)*sizeof(int))) == NULL ) {
 		fprintf(stderr, "Unable to allocate memory for colormap\n");
 		exit(EXIT_FAILURE);
 	}
-	for (i=0; i<nmax; i++) {
+	for (i=0; i<settings.nmax; i++) {
 		colormap[i] = rand()%(256*256*256); 
 	}
-	colormap[nmax] = 0;
+	colormap[settings.nmax] = 0;
 
 	screen = init_SDL();
 	p.x = -0.5; p.y = 0;
