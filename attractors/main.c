8703be562929b4ecf74dac04f2d94298d709964e
sebhz <seb@zyglute.fr>
2012-02-25 22:36:21 +0100

    [attractors] Proper acknowledgements

--- attractors/main.c
+++ attractors/main.c
@@ -2,19 +2,10 @@
  * This file accompanies the tutorial "Strange Attractors in C++ and OpenGL"
  * available at http://nathanselikoff.com/tutorial-strange-attractors-in-c-and-opengl
  * 
- * This application renders a Pickover strange attractor, "The King's Dream", the
- * parameters of which are found on page 27 of "Chaos in Wonderland" by Clifford Pickover
- * 
- * This file is released under Creative Commons Attribution Noncommercial Share-Alike 3.0 License
- * http://creativecommons.org/licenses/by-nc-sa/3.0/
- * You may modify this code, but you may not republish it without crediting me. Thank you.
- * 
- * Created by Nathan Selikoff on 4/13/11
- * http://nathanselikoff.com
+ * Copied from Nathan Selikoff code
+ * http://nathanselikoff.com/resources/tutorial-strange-attractors-in-c-and-opengl
  * 
  */
-
-
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
