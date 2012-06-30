702974ca1837210efb9c01f58192df876ca1cbcb
sebhz <seb@zyglute.fr>
2012-06-30 21:56:40 +0200

    [attractors] Move everything in one single file

--- attractors/attractors.c
+++ attractors/attractors.c
@@ -17,10 +17,70 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "attractors.h"
+#include <getopt.h>
 
+#ifdef __MINGW__
+#include <windows.h>
+#endif
+#include <GL/glut.h>
+
+#define VERSION_STRING "Polynomial strange attractors - version 1.0"
+#define DEFAULT_X 800
+#define DEFAULT_Y 600
+#define DEFAULT_POINTS 65536
+#define DEFAULT_ITER 8192
+#define DEFAULT_ORDER 2
+#define DEFAULT_DIM 3
 #define NUM_CONVERGENCE_POINTS 128
 #define AT_INFINITY 1000000
+
+typedef long double *point;
+
+struct lyapu
+{
+    long double lsum;
+    int n;
+    long double ly;
+};
+
+struct polynom
+{
+    long double *p[MDIM];
+    int length;
+    int order;
+};
+
+struct attractor
+{
+    struct polynom *polynom;
+    struct lyapu *lyapunov;
+    point *array;
+    int convergenceIterations;
+    int numPoints;
+    point bound[2];
+};
+
+typedef struct
+{
+    unsigned int numPoints;
+    unsigned int convergenceIterations;
+    unsigned int order;
+    unsigned int dimension;
+} fractal_settings_t;
+
+typedef struct
+{
+    unsigned long int w;        /* width of current window (in pixels) */
+    unsigned long int h;        /* height of current window (in pixels) */
+    int fullscreen;
+} display_settings_t;
+
+const char *WINDOW_TITLE = "Strange Attractors";
+static fractal_settings_t fset;
+static display_settings_t dset;
+static struct attractor *at;
+static float angle = 3.0;
+static double r = 0;
 
 /* Probably best algo as we are dealing with low exponents here (typically < 5)
      so no need to bother with exponentation by squaring */
@@ -302,9 +362,6 @@ explore (struct attractor *at, int order)
     }
 }
 
-#define min(x, y) (x) < (y)? (x) : (y)
-#define max(x, y) (x) > (y)? (x) : (y)
-
 void
 iterateMap (struct attractor *at)
 {
@@ -382,8 +439,6 @@ centerAttractor (struct attractor *at)
 struct attractor *
 newAttractor (void)
 {
-    struct attractor *at;
-
     if ((at = malloc (sizeof *at)) == NULL) {
         fprintf (stderr,
                  "Unable to allocate memory for attractor. Exiting\n");
@@ -404,4 +459,234 @@ newAttractor (void)
     iterateMap (at);
     centerAttractor (at);
     return at;
+}
+
+void
+usage (char *prog_name, FILE * stream)
+{
+}
+
+int
+numbers_from_string (long *num, char *s, char separator, int n)
+{
+    int i;
+    char *ss = s, *p = NULL;
+
+    for (i = 0; i < n; i++) {
+        num[i] = strtol (ss, &p, 0);
+        if ((num[i] == 0) && (p == ss)) {
+            return -1;
+        }
+        if (i == n - 1) {
+            break;
+        }
+        if (*p++ != separator) {
+            return -1;
+        }
+        ss = p;
+    }
+    return 0;
+}
+
+void
+parse_options (int argc, char **argv)
+{
+    int c;
+
+    while (1) {
+        static struct option long_options[] = {
+            {"conviter", required_argument, 0, 'c'},
+            {"dimension", required_argument, 0, 'd'},
+            {"fullscreen", no_argument, &dset.fullscreen, 1},
+            {"geometry", required_argument, 0, 'g'},
+            {"npoints", required_argument, 0, 'n'},
+            {"order", required_argument, 0, 'o'},
+            {"version", no_argument, 0, 'v'},
+            {0, 0, 0, 0}
+        };
+
+        /* getopt_long stores the option index here. */
+        int option_index = 0;
+        c = getopt_long (argc, argv, "c:d:fg:n:o:v", long_options,
+                         &option_index);
+
+        /* Detect the end of the options. */
+        if (c == -1)
+            break;
+        switch (c) {
+        case 0:
+            break;
+        case 'g':{
+                long n[2];
+                if (numbers_from_string (n, optarg, 'x', 2) == -1) {
+                    fprintf (stderr, "Bad geometry string\n");
+                    exit (EXIT_FAILURE);
+                }
+                dset.w = n[0];
+                dset.h = n[1];
+                break;
+            }
+
+        case 'h':
+            usage (argv[0], stdout);
+            exit (EXIT_SUCCESS);
+
+        case 'v':
+            fprintf (stdout, "%s\n", VERSION_STRING);
+            exit (EXIT_SUCCESS);
+            break;
+
+        case '?':
+            /* getopt_long already printed an error message. */
+            break;
+
+        default:
+            abort ();
+        }
+    }
+
+    /* Print any remaining command line arguments (not options). */
+    if (optind < argc) {
+        while (optind < argc)
+            fprintf (stderr,
+                     "%s is not recognized as a valid option or argument\n",
+                     argv[optind++]);
+        usage (argv[0], stderr);
+    }
+}
+
+void
+default_settings (void)
+{
+    dset.w = DEFAULT_X;
+    dset.h = DEFAULT_Y;
+    dset.fullscreen = 0;
+    fset.numPoints = DEFAULT_POINTS;
+    fset.convergenceIterations = DEFAULT_ITER;
+    fset.order = DEFAULT_ORDER;
+    fset.dimension = DEFAULT_DIM;
+}
+
+void
+initDisplay ()
+{
+    int i;
+
+    glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
+    glColor4f (1.0f, 1.0f, 1.0f, 0.02f);
+    glViewport (0, 0, dset.w, dset.h);
+
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+
+    /* The square of the diagonal length */
+    for (i = 0; i < MDIM; i++) {
+        r += (at->bound[1][i] - at->bound[0][i]) * (at->bound[1][i] -
+                                                    at->bound[0][i]);
+    }
+    /* Add 2% to move the clipping planes a bit further */
+    r = 0.51 * sqrt (r);
+    glOrtho (-r, r, -r, r, -r, r);
+
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity ();
+
+#if (MDIM == 2)
+    glEnable (GL_BLEND);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+#endif
+
+    glEnable (GL_POINT_SMOOTH);
+    glPointSize (1.0f);
+}
+
+void
+display ()
+{
+    int i;
+
+    glClear (GL_COLOR_BUFFER_BIT);
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity ();
+    glRotatef (angle, 1.0, 1.0, 1.0);
+
+    glBegin (GL_POINTS);
+    for (i = 0; i < at->numPoints; i++) {
+#if (MDIM == 2)
+        glVertex2f (at->array[i][0], at->array[i][1]);
+#else
+        glVertex3f (at->array[i][0], at->array[i][1], at->array[i][2]);
+#endif
+    }
+    glEnd ();
+
+    glutSwapBuffers ();
+    angle += 1.0;
+}
+
+/* Keep aspect ratio */
+void
+reshape (int w, int h)
+{
+    GLdouble ar;
+
+    glViewport (0, 0, w, h);
+    ar = (GLdouble) w / (GLdouble) h;
+
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+
+    if (ar < 1.0) {
+        glOrtho (-r, r, -r / ar, r / ar, -r, r);
+    }
+    else {
+        glOrtho (-r * ar, r * ar, -r, r, -r, r);
+    }
+    glMatrixMode (GL_MODELVIEW);
+    glLoadIdentity ();
+}
+
+void
+key (unsigned char mychar, int x, int y)
+{
+    // exit the program when the Esc key is pressed
+    if (mychar == 27) {
+        exit (0);
+    }
+}
+
+void
+animate (int argc, char **argv)
+{
+    glutInit (&argc, argv);
+    glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+
+    glutInitWindowSize (dset.w, dset.h);
+    glutCreateWindow (WINDOW_TITLE);
+
+    /* Even if there are no events, redraw our gl scene. */
+    glutIdleFunc (display);
+    glutDisplayFunc (display);
+    glutKeyboardFunc (key);
+    glutReshapeFunc (reshape);
+
+    initDisplay ();
+    glutMainLoop ();
+}
+
+int
+main (int argc, char **argv)
+{
+#ifdef __MINGW__
+    freopen ("CON", "w", stdout);
+    freopen ("CON", "w", stderr);
+#endif
+
+    srand (time (NULL));
+    default_settings ();
+    parse_options (argc, argv);
+    at = newAttractor ();
+    animate (argc, argv);
+    freeAttractor (at);
+    return EXIT_SUCCESS;
 }
