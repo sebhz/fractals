0fadfa0a7ec15981ce0a9e72c30207994eca8c2d
sebhz <seb@zyglute.fr>
2016-08-29 11:29:18 +0200

    [attractor web] Use Image MIME type for attachment.
    Signed-off-by: sebhz <seb@zyglute.fr>

--- attractors/python/web/create_daily.py
+++ attractors/python/web/create_daily.py
@@ -9,7 +9,7 @@ from time import time
 from random import randint
 from datetime import datetime, timedelta
 from os.path import basename
-from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
@@ -215,9 +215,9 @@ def send_mail(MAP, server, send_from, send_to, subject, files=None):
 
 	for f in files or []:
 		with open(f, "rb") as fil:
-			part = MIMEApplication(
+			part = MIMEImage(
 				fil.read(),
-				Name=basename(f)
+				"png"
 			)
 			part['Content-Disposition'] = 'attachment; filename=%s' % basename(f)
 			msg.attach(part)
