b71ba5a31806eafef74397bd7b8e7c82c21b614c
sebhz <seb@zyglute.fr>
2019-02-12 13:25:37 +0100

    [attractors] Added coloring
    Used same scheme as for Mandelbrot (ramp mapping
    and basic RGB coef).
    
    Signed-off-by: sebhz <seb@zyglute.fr>

--- attractors/python/attractor/render.py
+++ attractors/python/attractor/render.py
@@ -1,164 +1,193 @@
 #!/usr/bin/python
 
-import colorsys
-import random
 import logging
+import random
+import math
 
 try:
-	import png
+    import png
 except:
-	import sys
-	print("this program requires the pyPNG module", file=sys.stderr)
-	print("available at https://github.com/drj11/pypng", file=sys.stderr)
-	raise SystemExit
+    import sys
+    print("this program requires the pyPNG module", file=sys.stderr)
+    print("available at https://github.com/drj11/pypng", file=sys.stderr)
+    raise SystemExit
 
-INTERNAL_BPC=16
 
 defaultParameters = {
-	'transparentbg': False,
-	'colormode': 'light',
-	'subsample': 1,
-	'bpc' : 8,
-	'dimension' : 2,
-	'geometry' : (800, 600),
+    'transparentbg': False,
+    'colormode': 'light',
+    'subsample': 1,
+    'bpc' : 8,
+    'dimension' : 2,
+    'geometry' : (800, 600),
 }
 
 class Renderer(object):
-	def __init__(self, **kwargs):
-		getParam = lambda k: kwargs[k] if kwargs and k in kwargs else defaultParameters[k]
+    def __init__(self, **kwargs):
+        getParam = lambda k: kwargs[k] if kwargs and k in kwargs else defaultParameters[k]
 
-		self.logger     = logging.getLogger(__name__)
-		self.bpc        = getParam('bpc')
-		self.subsample  = getParam('subsample')
-		self.geometry   = getParam('geometry')
-		self.colormode  = getParam('colormode')
-		self.dimension  = getParam('dimension')
-		self.transparentbg  = getParam('transparentbg')
-		self.shift      = INTERNAL_BPC - self.bpc
-		self.backgroundColor = (1<<self.bpc) - 1 if self.colormode == 'light' else 0
-		self.geometry   = [x*self.subsample for x in self.geometry]
-		self.internalbg = 0xFFFF if self.colormode == 'light' else 0
-		if self.dimension < 2 or self.dimension > 3:
-			self.logger.warning("Trying to create renderer with invalid dimension (" + self.dimension + "). Defaulting to 2.")
-			self.dimension = 2
+        self.logger     = logging.getLogger(__name__)
+        self.bpc        = getParam('bpc')
+        self.subsample  = getParam('subsample')
+        self.geometry   = getParam('geometry')
+        self.colormode  = getParam('colormode')
+        self.dimension  = getParam('dimension')
+        self.transparentbg  = getParam('transparentbg')
+        self.backgroundColor = (1<<self.bpc) - 1 if self.colormode == 'light' else 0
+        self.geometry   = [x*self.subsample for x in self.geometry]
+        self.internalbg = 0xFFFF if self.colormode == 'light' else 0
+        if self.dimension < 2 or self.dimension > 3:
+            self.logger.warning("Trying to create renderer with invalid dimension (" + self.dimension + "). Defaulting to 2.")
+            self.dimension = 2
 
-	# Equalize the attractor
-	# attractor: attractor points: dict (X,Y) and containing :
-	# - frequency for 2D
-	# - Z for 3D
-	# Returns the attractor points: dict indexed by (X, Y) and containing COLOR, 
-	def postprocessAttractor(self, at):
-		M = max(at.values())
+        if self.colormode == 'color':
+            self.INTERNAL_BPC=11
+        else:
+            self.INTERNAL_BPC=16
 
-		# Now send the map in the [0, (1<<INTERNAL_BPC)-1] range
-		for i, pt in at.items():
-			at[i] = int (pt * ((1<<INTERNAL_BPC)-1)/M)
+        self.shift      = self.INTERNAL_BPC - self.bpc
 
-		# Equalize the attractor (histogram equalization)
-		self.equalizeAttractor(at)
+    # Equalize the attractor
+    # attractor: attractor points: dict (X,Y) and containing :
+    # - frequency for 2D
+    # - Z for 3D
+    # Returns the attractor points: dict indexed by (X, Y) and containing COLOR, 
+    def postprocessAttractor(self, at):
+        M = max(at.values())
 
-		# Subsample here
-		at = self.subsampleAttractor(at)
+        # Now send the map in the [0, (1<<self.INTERNAL_BPC)-1] range
+        for i, pt in at.items():
+            at[i] = int (pt * ((1<<self.INTERNAL_BPC)-1)/M)
 
-		return at
+        # Equalize the attractor (histogram equalization)
+        self.equalizeAttractor(at)
 
-	# Creates the final image array
-	def createImageArray(self, p, sd):
-		w = int ((sd[0])/self.subsample)
-		h = int ((sd[1])/self.subsample)
+        # Subsample here
+        at = self.subsampleAttractor(at)
 
-		a = [self.backgroundColor]*w*h
+        return at
 
-		for c, v in p.items():
-			offset = c[0] + c[1]*w
-			try:
-				a[offset] = int(v) >> self.shift
-			except IndexError as e:
-				# This can occur if the bounds were not correctly assessed
-				# and a point of the atractor happens to fall out of them.
-				logging.debug("Looks like a point fell out of our bounds. Ignoring it")
-		return a
+    def colorize_pixel(self, v, coef):
+        # Ramp mapping 0->512 to 0->256
+        per_color = lambda x: 128 + x if x < 128 else 383 - x if x < 384 else x - 384
 
-	# Performs histogram equalization on the attractor pixels
-	def equalizeAttractor(self, p):
-		pools = [0]*(1<<INTERNAL_BPC)
+        sqv = 2*math.sqrt(v)
+        r = per_color(int(math.floor(sqv * coef[0])) % 512)
+        g = per_color(int(math.floor(sqv * coef[1])) % 512)
+        b = per_color(int(math.floor(sqv * coef[2])) % 512)
 
-		# Create cumulative distribution
-		for v in p.values():
-			pools[v] += 1
-		for i in range(len(pools) - 1):
-			pools[i+1] += pools[i]
+        return (r, g, b,)
 
-		# Stretch the values to the [1, (1<<INTERNAL_BPC)-1] range
-		for i, v in enumerate(pools):
-			pools[i] = 1+((1<<INTERNAL_BPC)-2)*(pools[i]-pools[0])/(pools[-1]-pools[0])
+    # Creates the final image array
+    def createImageArray(self, p, sd):
+        w = int ((sd[0])/self.subsample)
+        h = int ((sd[1])/self.subsample)
 
-		# Now reapply the stretched values
-		if self.dimension == 2:
-			for k in p:
-				if self.colormode == 'light': # invert the values so that high order pixels are dark
-					p[k] = ((1<<INTERNAL_BPC)-1) - pools[p[k]]
-				else:
-					p[k] = pools[p[k]]
-		else:
-			for k in p:
-				p[k] = pools[p[k]]
+        coef = (random.randint(1,4), random.randint(1,4), random.randint(1,4),)
+        if self.colormode == 'color':
+            logging.debug("Color coefficients: (R, G, B) = (%d, %d, %d)" % coef)
+            a = [ (self.backgroundColor, self.backgroundColor, self.backgroundColor) ]*w*h
+        else:
+            a = [ self.backgroundColor ]*w*h
 
-	def subsampleAttractor(self, at):
+        for c, v in p.items():
+            offset = c[0] + c[1]*w
+            try:
+                if self.colormode == 'color':
+                    a[offset] = self.colorize_pixel(v, coef)
+                else:
+                    a[offset] = int(v) >> self.shift
+            except IndexError as e:
+                # This can occur if the bounds were not correctly assessed
+                # and a point of the attractor happens to fall out of them.
+                logging.debug("Looks like a point fell out of our bounds. Ignoring it.")
+        return a
 
-		if self.subsample == 1: return at
+    # Performs histogram equalization on the attractor pixels
+    def equalizeAttractor(self, p):
+        pools = [0]*(1<<self.INTERNAL_BPC)
 
-		nat = dict()
+        # Create cumulative distribution
+        for v in p.values():
+            pools[v] += 1
+        for i in range(len(pools) - 1):
+            pools[i+1] += pools[i]
 
-		for pt, color in at.items():
-			xsub = int(pt[0]/self.subsample)
-			ysub = int(pt[1]/self.subsample)
-			if (xsub,ysub) in nat: # We already subsampled this square
-				continue
-			n = 0
-			c = 0
-			x0 = xsub*self.subsample
-			y0 = ysub*self.subsample
-			for x in range(x0, x0+self.subsample):
-				for y in range(y0, y0+self.subsample):
-					if (x, y) in at:
-						n += 1
-						c += at[(x, y)]
-			# OK now we have accumulated all colors in the attractors
-			# Time to weight with the background color
-			v = self.internalbg*(self.subsample*self.subsample-n)
-			c += v
-			c = int(c/(self.subsample*self.subsample))
-			nat[(xsub,ysub)] = c
+        # Stretch the values to the [1, (1<<INTERNAL_BPC)-1] range
+        for i, v in enumerate(pools):
+            pools[i] = 1+((1<<self.INTERNAL_BPC)-2)*(pools[i]-pools[0])/(pools[-1]-pools[0])
 
-		return nat
+        # Now reapply the stretched values
+        if self.dimension == 2:
+            for k in p:
+                if self.colormode == 'light': # invert the values so that high order pixels are dark
+                    p[k] = ((1<<self.INTERNAL_BPC)-1) - pools[p[k]]
+                else:
+                    p[k] = pools[p[k]]
+        else:
+            for k in p:
+                p[k] = pools[p[k]]
 
-	def renderAttractor(self, a):
-		if not a: return None
-		p = self.postprocessAttractor(a)
-		i = self.createImageArray(p, self.geometry)
-		return i
+    def subsampleAttractor(self, at):
 
-	def writeAttractorPNG(self, a, filepath):
-		self.logger.debug("Now writing attractor %s on disk." % filepath)
+        if self.subsample == 1: return at
 
-		w = png.Writer(size=[int(x/self.subsample) for x in self.geometry], greyscale = True, bitdepth=self.bpc, interlace=True, transparent = self.backgroundColor if self.transparentbg else None)
-		aa = w.array_scanlines(a)
-		with open(filepath, "wb") as f:
-			w.write(f, aa)
+        nat = dict()
 
-	def isNice(self, a, coverLimit=0.01):
-		"""
-		Checks if the attractor passed is 'nice': currently nice means that the
-		attractors covers more than coverLimit percent of the window.
-		"""
-		if not a: return False
-		nPoints = len(a)
-		sSize   = self.geometry[0]*self.geometry[1]
-		coverRatio = float(nPoints)/sSize
-		self.logger.debug("Attractor cover ratio is %.2f%%" % (100.0*coverRatio))
-		if coverRatio < coverLimit:
-			return False
+        for pt, color in at.items():
+            xsub = int(pt[0]/self.subsample)
+            ysub = int(pt[1]/self.subsample)
+            if (xsub,ysub) in nat: # We already subsampled this square
+                continue
+            n = 0
+            c = 0
+            x0 = xsub*self.subsample
+            y0 = ysub*self.subsample
+            for x in range(x0, x0+self.subsample):
+                for y in range(y0, y0+self.subsample):
+                    if (x, y) in at:
+                        n += 1
+                        c += at[(x, y)]
+            # OK now we have accumulated all colors in the attractors
+            # Time to weight with the background color
+            v = self.internalbg*(self.subsample*self.subsample-n)
+            c += v
+            c = int(c/(self.subsample*self.subsample))
+            nat[(xsub,ysub)] = c
 
-		return True
+        return nat
+
+    def renderAttractor(self, a):
+        if not a: return None
+        p = self.postprocessAttractor(a)
+        i = self.createImageArray(p, self.geometry)
+        return i
+
+    def writeAttractorPNG(self, a, filepath):
+        self.logger.debug("Now writing attractor %s on disk." % filepath)
+
+        if self.colormode == 'color':
+            w = png.Writer(size=[int(x/self.subsample) for x in self.geometry], bitdepth=self.bpc, interlace=True, transparent = self.backgroundColor if self.transparentbg else None)
+            aa = a
+        else:
+            w = png.Writer(size=[int(x/self.subsample) for x in self.geometry], greyscale = True, bitdepth=self.bpc, interlace=True, transparent = self.backgroundColor if self.transparentbg else None)
+            aa = w.array_scanlines(a)
+
+        with open(filepath, "wb") as f:
+            w.write(f, aa)
+
+    def isNice(self, a, coverLimit=0.01):
+        """
+        Checks if the attractor passed is 'nice': currently nice means that the
+        attractors covers more than coverLimit percent of the window.
+        """
+        if not a: return False
+        nPoints = len(a)
+        sSize   = self.geometry[0]*self.geometry[1]
+        coverRatio = float(nPoints)/sSize
+        self.logger.debug("Attractor cover ratio is %.2f%%" % (100.0*coverRatio))
+        if coverRatio < coverLimit:
+            return False
+
+        return True
 
