d2533713a7e3017659b8fa5e9d606febab8fabd2
sebhz <seb@zyglute.fr>
2020-04-05 16:57:59 +0200

    [attractors] Simplifications and improvements
        - Factorize gradient generation code
        - Gradient visualizer now has proper background

--- attractors/python/attractor/palettes.py
+++ attractors/python/attractor/palettes.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import colorsys
+
+rgb_norm = lambda x: (((x>>16)&0xFF)/0xFF, ((x>>8)&0xFF)/0xFF, ((x>>0)&0xFF)/0xFF,)
 
 # All colors are coded in RGB888. Colorspace indication gives the type of gradient to generate.
 pal_templates = (
@@ -84,4 +87,39 @@ pal_templates = (
       'gradient_map' : [ { 'slice_percent': 100, 'start_color': 0x66008F, 'end_color': 0xFEA610 } ]
     },
 )
+
+def getGradientSlice(s, n, grad_type='hsv_cw', out_space='hsv'):
+    grad_space = grad_type[0:3]
+    grad_dir   = grad_type[4:6]
+
+    (start_color, end_color) = map(rgb_norm, (s['start_color'], s['end_color']))
+    ns  = round(n*s['slice_percent']/100)
+    if grad_space == 'hsv':
+        (start_color, end_color) = (colorsys.rgb_to_hsv(*start_color), colorsys.rgb_to_hsv(*end_color))
+        inc = [(x-y)/ns for (x, y) in zip(end_color, start_color)]
+        # HSV gradients hue can go clockwise (red towards green) or counterclockwise (red towards blue)
+        delta_hue = end_color[0]-start_color[0]
+        if (delta_hue > 0 and grad_dir == 'cc'): inc[0] = (1-delta_hue)/ns
+        if (delta_hue < 0 and grad_dir == 'cw'): inc[0] = (1+delta_hue)/ns
+    else:
+        inc = [(x-y)/ns for (x, y) in zip(end_color, start_color)]
+
+    gs = list()
+
+    for i in range(ns):
+        cur_color = [ x+i*y for (x,y) in zip(start_color, inc) ]
+        if grad_space == out_space:
+            out_color = cur_color
+        elif out_space == 'rgb':
+            out_color = colorsys.hsv_to_rgb(*cur_color)
+        else:
+            out_color = colorsys.rgb_to_hsv(*cur_color)
+        gs.append(tuple(out_color))
+    return gs
+
+def getGradient(m, n, grad_type='hsv_cw', out_space='hsv'):
+    g = list()
+    for s in m:
+        g += getGradientSlice(s, n, grad_type, out_space)
+    return g
 
--- attractors/python/attractor/render.py
+++ attractors/python/attractor/render.py
@@ -17,8 +17,6 @@ defaultParameters = {
     'paletteIndex': None
 }
 
-rgb_norm = lambda x: (((x>>16)&0xFF)/0xFF, ((x>>8)&0xFF)/0xFF, ((x>>0)&0xFF)/0xFF,)
-
 class Renderer(object):
     def __init__(self, **kwargs):
         getParam = lambda k: kwargs[k] if kwargs and k in kwargs else defaultParameters[k]
@@ -38,45 +36,10 @@ class Renderer(object):
         if self.paletteIndex == None:
             self.paletteIndex = random.choice(range(len(palettes.pal_templates)))
 
-    def getGradientSlice(self,s, n, grad_type='hsv_cw', out_space='hsv'):
-        grad_space = grad_type[0:3]
-        grad_dir   = grad_type[4:6]
-
-        (start_color, end_color) = map(rgb_norm, (s['start_color'], s['end_color']))
-        ns  = round(n*s['slice_percent']/100)
-        if grad_space == 'hsv':
-            (start_color, end_color) = (colorsys.rgb_to_hsv(*start_color), colorsys.rgb_to_hsv(*end_color))
-            inc = [(x-y)/ns for (x, y) in zip(end_color, start_color)]
-            # HSV gradients hue can go clockwise (red towards green) or counterclockwise (red towards blue)
-            delta_hue = end_color[0]-start_color[0]
-            if (delta_hue > 0 and grad_dir == 'cc'): inc[0] = (1-delta_hue)/ns
-            if (delta_hue < 0 and grad_dir == 'cw'): inc[0] = (1+delta_hue)/ns
-        else:
-            inc = [(x-y)/ns for (x, y) in zip(end_color, start_color)]
-
-        gs = list()
-
-        for i in range(ns):
-            cur_color = [ x+i*y for (x,y) in zip(start_color, inc) ]
-            if grad_space == out_space:
-                out_color = cur_color
-            elif out_space == 'rgb':
-                out_color = colorsys.hsv_to_rgb(*cur_color)
-            else:
-                out_color = colorsys.rgb_to_hsv(*cur_color)
-            gs.append(tuple(out_color))
-        return gs
-
-    def getGradient(self, m, n, grad_type='hsv_cw', out_space='hsv'):
-        g = list()
-        for s in m:
-            g += self.getGradientSlice(s, n, grad_type, out_space)
-        return g
-
     def getPalette(self, frequencies):
         self.logger.debug("Choosing palette %d (%s)" % (self.paletteIndex, palettes.pal_templates[self.paletteIndex]['name']))
         template = palettes.pal_templates[self.paletteIndex]
-        gradient = self.getGradient(template['gradient_map'], len(frequencies), template['colorspace'])
+        gradient = palettes.getGradient(template['gradient_map'], len(frequencies), template['colorspace'])
         while len(gradient) < len(frequencies):
             gradient.append(gradient[-1])
 
@@ -89,7 +52,7 @@ class Renderer(object):
             colormap[freq] = tuple([round(x*((1<<self.bpc)-1)) for x in (b, g, r)])
 
         self.palette = dict()
-        self.palette['background'] = tuple(reversed([round(component * ((1 << self.bpc)-1)) for component in rgb_norm(template['background'])]))
+        self.palette['background'] = tuple(reversed([round(component * ((1 << self.bpc)-1)) for component in palettes.rgb_norm(template['background'])]))
         self.palette['colormap']   = colormap
 
     def colorizeAttractor(self, p):
