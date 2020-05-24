199c876cb9942908900b1f84597609907fbbd217
sebhz <seb@zyglute.fr>
2020-05-24 16:52:50 +0200

    [attractors] More style improvements

--- attractors/python/gradient.py
+++ attractors/python/gradient.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-
-import numpy as np
+""" Simple program to test color gradients.
+    Uses the palettes module of the attractors
+"""
 import colorsys
+import numpy as np
 from matplotlib import pyplot as plt
 from attractor import palettes
 
-def mapValueGradient(hsv_gradient, nc, offset, invert):
-    for i, v in enumerate(hsv_gradient):
-        l = list(v)
-        l[2] = offset + (1.0-offset)*i/nc
-        if invert: l[2] = 1.0-l[2]
-        hsv_gradient[i] = tuple(l)
+def map_gradient(hsv_g, num_c, offset, invert):
+    """
+    Maps a gradient to a number of colors (by stretching the
+    value component, applying an offset and possibly inverting)
+    """
+    for index, components in enumerate(hsv_g):
+        cmp_l = list(components)
+        cmp_l[2] = offset + (1.0-offset)*index/num_c
+        if invert:
+            cmp_l[2] = 1.0-cmp_l[2]
+        hsv_g[index] = tuple(cmp_l)
 
-nc = 768
+NUM_COLORS = 768
 for i, template in enumerate(palettes.pal_templates):
-    hsv_gradient = palettes.getGradient(template['gradient_map'], nc, template['colorspace'], 'hsv')
-    mapValueGradient(hsv_gradient, nc, template['value_offset'], template['invert_value'])
-    rgb_gradient = [ tuple([ round(255*component) for component in colorsys.hsv_to_rgb(*color)]) for color in hsv_gradient ]
+    hsv_gradient = palettes.getGradient(template['gradient_map'],
+                                        NUM_COLORS,
+                                        template['colorspace'],
+                                        'hsv')
+    map_gradient(hsv_gradient, NUM_COLORS, template['value_offset'], template['invert_value'])
+    rgb_gradient = [tuple([round(255*component) for component in colorsys.hsv_to_rgb(*color)])
+                    for color in hsv_gradient]
 
     img_array = [rgb_gradient]*128
     print("Palette %d (%s)" % (i, template['name']))
